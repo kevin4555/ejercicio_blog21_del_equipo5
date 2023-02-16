5cd521d3f62fab3e70bb8c27130210ad4041fd09
KChino <kevintlapizaga@gmail.com>
2023-02-16 10:41:18 -0300

    Update dbInitialSetup.js

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -2,10 +2,10 @@ const db = require("./models");
 
 module.exports = async () => {
   // Crear tablas:
-  // await db.sequelize.sync({ force: true });
-  /*   console.log("[Database] ¡Las tablas fueron creadas!"); */
+  await db.sequelize.sync({ force: true });
+  console.log("[Database] ¡Las tablas fueron creadas!");
 
   // Ejecutar seeders (datos de prueba):
   await require("./seeders/articleSeeder")();
-  /*   console.log("[Database] ¡Los datos de prueba fueron insertados!"); */
+  console.log("[Database] ¡Los datos de prueba fueron insertados!");
 };
